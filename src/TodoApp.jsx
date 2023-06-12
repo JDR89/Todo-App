86467af62b41ac855ace45ffd04b2307fbee9547
JDR89 <joaquin.romero89@gmail.com>
2023-06-11 21:31:25 -0300

    arreglando progress cuando se elimina un done true

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,78 +1,77 @@
-
 import { useState } from "react";
 import { AddTodo } from "./components/AddTodo";
 import ItemTodo from "./components/ItemTodo";
 
 export const TodoApp = () => {
+  const [arregloTodos, setArregloTodos] = useState([]);
+  const [progress, setProgress] = useState(0);
+  
+  
 
-  const [arregloTodos, setArregloTodos] = useState([])
-  const [progress, setProgress ] = useState(0)
-
-
-
-  const onAdd=(todo)=>{
-    
+  const onAdd = (todo) => {
     const newTodo = {
-      text:todo,
-      id:Math.floor(Math.random() * 10000),
-      done:false
-    }
+      text: todo,
+      id: Math.floor(Math.random() * 10000),
+      done: false,
+    };
 
-    if(newTodo.text.length<3)return
-    setArregloTodos([...arregloTodos,newTodo])
+    if (newTodo.text.length < 3) return;
+
+    setArregloTodos([...arregloTodos, newTodo]);
+  };
+
+  const onDelete = (id) => {
+    const newArray = arregloTodos.filter((todo) => todo.id !== id);
+    setArregloTodos(newArray);
     
-  }
-  
-  const onDelete=(id)=>{
+  };
 
-    const newArray = arregloTodos.filter(todo=>todo.id!==id)
-    setArregloTodos(newArray)
+  const isDone = (id) => {
+    const todoIndex = arregloTodos.findIndex((todo) => todo.id === id);
 
-  }
+    if (todoIndex !== -1) {
+      arregloTodos[todoIndex].done = !arregloTodos[todoIndex].done;
+    }
+    
+  };
 
-  const isDone=(id)=>{
+  const onProgress = () => {
+    const completedTodos = arregloTodos.filter((todo) => todo.done).length;
+    const totalTodo = arregloTodos.length;
+    const progressPercentage =
+      totalTodo > 0 ? (completedTodos / totalTodo) * 100 : 0;
+      const roundedProgress = Math.round(progressPercentage)
+    setProgress(roundedProgress);
+  };
 
-   const todoIndex = arregloTodos.findIndex(todo=>todo.id===id)
-
-   if (todoIndex !== -1) {
-    arregloTodos[todoIndex].done = !arregloTodos[todoIndex].done;
-  } 
-  
-  }
-
-  const onProgress =()=>{
-    const completedTodos = arregloTodos.filter(todo => todo.done).length;
-     const totalTodo = arregloTodos.length;
-      const progressPercentage = (completedTodos / totalTodo) * 100;
-        const total = Math.round(progressPercentage)
-  setProgress(total);
-  }
-
- 
-  
-  
   return (
-    <div  className=" min-h-screen p-5 bg-[#353535]">
-
+    <div className=" min-h-screen p-5 bg-[#353535]">
       <div className=" flex justify-center items-center">
-       <AddTodo onAdd={onAdd} />
+        <AddTodo onAdd={onAdd} />
 
-       <div className="ms-3">
-       <div className={`radial-progress bg-primary text-primary-content border-4 border-primary`} style={{ "--value": progress }}>{progress}%</div>
-       </div>
-
+        <div className="ms-3">
+          <div
+            className={`radial-progress bg-primary text-primary-content border-4 border-primary`}
+            style={{ "--value": progress }}
+          >
+            {progress}%
+          </div>
+        </div>
       </div>
-        
-      
+
       <div className="mt-5 flex justify-center">
         <ul className=" w-full sm:w-1/2 p-4 flex flex-col items-center text-white">
-        {
-          arregloTodos.map(todo=><ItemTodo key={todo.id} todo={todo} onDelete={onDelete} isDone={isDone} onProgress={onProgress} />)
-        }
+          {arregloTodos.map((todo) => (
+            <ItemTodo
+              key={todo.id}
+              todo={todo}
+              onDelete={onDelete}
+              isDone={isDone}
+              onProgress={onProgress}
+            />
+          ))}
         </ul>
       </div>
-
-
     </div>
   );
 };
